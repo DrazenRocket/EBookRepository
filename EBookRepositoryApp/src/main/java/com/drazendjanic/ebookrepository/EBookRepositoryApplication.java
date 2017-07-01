faa669e4d3aee762765ae4f1612b6e061c7543ac
DrazenRocket <drazen.rocket@hotmail.com>
2017-07-01 20:52:28 +0200

    Created a project.

---
+++ EBookRepositoryApp/src/main/java/com/drazendjanic/ebookrepository/EBookRepositoryApplication.java
@@ -0,0 +1,12 @@
+package com.drazendjanic.ebookrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBookRepositoryApplication.class, args);
+	}
+}
---
+++ EBookRepositoryApp/src/main/java/com/drazendjanic/ebookrepository/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.drazendjanic.ebookrepository;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EBookRepositoryApplication.class);
+	}
+
+}
