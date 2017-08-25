135a621792bc173bb6e71c733f8b95fb770b6079
DrazenRocket <drazen.rocket@hotmail.com>
2017-08-25 16:35:56 +0200

    Refactored code for featura 'User's profile'.

--- EBookRepositoryApp/src/main/webapp/app/components/user/user-profile/user-profile.controller.js
+++ EBookRepositoryApp/src/main/webapp/app/components/user/user-profile/user-profile.controller.js
@@ -9,26 +9,32 @@
     function UserProfileController(userService, $state, $stateParams, $window) {
         var viewModel = this;
 
-        viewModel.loggedInUserId = userService.getUserIdFromLocalStorage();
+        viewModel.loggedIn = userService.isLoggedIn();
+        viewModel.loggedInUserId = -1;
         viewModel.loggedInUserDetails = {};
         viewModel.userId = $stateParams.userId;
         viewModel.userDetails = {};
-        viewModel.isOfLoggedInUser = viewModel.userId == viewModel.loggedInUserId;
+        viewModel.isOfLoggedInUser = false;
         viewModel.updateProfile = updateProfile;
         viewModel.changePassword = changePassword;
+
+        if (viewModel.loggedIn) {
+            viewModel.loggedInUserId = userService.getUserIdFromLocalStorage();
+            viewModel.isOfLoggedInUser = viewModel.userId == viewModel.loggedInUserId;
+            userService
+                .getUserById(viewModel.loggedInUserId)
+                .then(function (response) {
+                    if (response.status == 200) {
+                        viewModel.loggedInUserDetails = response.data;
+                    }
+                });
+        }
 
         userService
             .getUserById(viewModel.userId)
             .then(function (response) {
                 if (response.status == 200) {
                     viewModel.userDetails = response.data;
-                }
-            });
-        userService
-            .getUserById(viewModel.loggedInUserId)
-            .then(function (response) {
-                if (response.status == 200) {
-                    viewModel.loggedInUserDetails = response.data;
                 }
             });
 
